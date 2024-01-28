304bad861af9fc8cf6efc5cbd40003cde6c1b163
Davlatov Shahzod <shahzod@davlatov.space>
2024-01-28 18:00:23 +0300

    Setup linters

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true,
+    strict: true,
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './components/**/*.{vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{ts}',
+    './app.vue',
+    './error.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
