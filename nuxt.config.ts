ca33c207f0b8fcfbb50dfed5560a20406c268dcf
Davlatov Shahzod <shahzod@davlatov.space>
2024-03-08 22:48:06 +0300

    Off type check on build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   typescript: {
-    typeCheck: 'build',
+    typeCheck: false,
     strict: true,
   },
   css: ['~/assets/css/main.css'],
