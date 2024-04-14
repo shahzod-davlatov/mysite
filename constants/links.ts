172ede2c1974c35c18ff0baeb1e9c0173c24ec5f
Davlatov Shahzod <shahzod@davlatov.space>
2024-04-14 15:23:11 +0300

    Change github link

--- constants/links.ts
+++ constants/links.ts
@@ -1,6 +1,6 @@
 export const TELEGRAM_LINK = 'https://t.me/shahzod418';
 export const VK_LINK = 'https://vk.com/shahzod418';
-export const GITHUB_LINK = 'https://github.com/shahzod418';
+export const GITHUB_LINK = 'https://github.com/shahzod-davlatov';
 
 export const MAIN_PAGE_LINK = '/';
 export const ABOUT_ME_PAGE_LINK = '/about-me';
