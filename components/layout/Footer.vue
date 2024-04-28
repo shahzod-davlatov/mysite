508c74fe4d5ecfa9807f859657c6b25a8ad5345d
Davlatov Shahzod <shahzod@davlatov.space>
2024-04-28 13:46:30 +0300

    Update footer

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -30,21 +30,13 @@ const linksMap = [
 </script>
 
 <template>
-  <div class="col-span-full flex border-t border-line">
-    <div
-      class="flex h-full grow items-center border-r border-line px-4 md:grow-0 md:pl-6"
-    >
-      <span class="text-label text-secondary">{{ t('findMe') }}</span>
-    </div>
+  <div class="col-span-full flex justify-end border-t border-line">
     <LayoutLink
-      v-for="({ to, group, groupHover, icon }, index) in linksMap"
+      v-for="{ to, group, groupHover, icon } in linksMap"
       :key="group"
-      :class="[
-        group,
-        { 'border-r-0 md:border-r': index === linksMap.length - 1 },
-      ]"
+      :class="group"
       :href="to"
-      class="w-14 border-r border-line"
+      class="w-14 border-l border-line"
       target="_blank"
     >
       <component :is="icon" :class="groupHover" class="btn-social transition" />
